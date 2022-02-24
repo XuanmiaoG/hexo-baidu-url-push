4dc3d279e2085dfa981df71434eb67cbb195b260
XuanmiaoG <gaoy31@gmail.com>
2022-02-24 10:27:42 +0800

    fix bug issue#1“

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 hexo.extend.filter.register('before_post_render', function(data){
-    var file_content = fs.readFileSync(path.join(_dirname,"push.js"),"utf-8");
+    var file_content = fs.readFileSync('./node_modules/hexo-baidu-url-push/push.js');
         data.content += file_content;
     return data;
 });
